022daa3e190e7338b8da619e680d7bed816665b5
arfstudy <147665859+arfstudy@users.noreply.github.com>
2024-05-16 20:25:28 +0500

    Восстановливает пароль.
    Применяет собственные шаблоны страниц запроса восстановления, об отправки
    письма, ввода нового пароля и успешности выполнения.
    Для применения всех этих шаблонов переопределил представления 'PasswordResetView'
    и 'PasswordResetConfirmView'. Вызывает их явно
    
    Changes committed:
        1. modified:   reference/my_diplom/templates/registration/login.html
        2. new file:   reference/my_diplom/templates/registration/password_reset_complete.html
        3. new file:   reference/my_diplom/templates/registration/password_reset_confirm.html
        4. new file:   reference/my_diplom/templates/registration/password_reset_done.html
        5. new file:   reference/my_diplom/templates/registration/password_reset_email.html
        6. new file:   reference/my_diplom/templates/registration/password_reset_form.html
        7. modified:   reference/my_diplom/webauth/urls.py
        8. modified:   reference/my_diplom/webauth/views.py

--- reference/my_diplom/webauth/urls.py
+++ reference/my_diplom/webauth/urls.py
@@ -4,7 +4,7 @@ URL configuration for users application.
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 """
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 from webauth import views
@@ -38,6 +38,18 @@ urlpatterns = [
         TemplateView.as_view(template_name='webauth/user_delete_complete.html'),
         name='user_delete_complete',
     ),
+    path("password_reset/", views.AppPasswordResetView.as_view(), name="password_reset"),
+    path("reset/<uidb64>/<token>/",views.AppPasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('', include(arg='django.contrib.auth.urls')),
+    # Находится: 700_Diplom/700_venv/lib/python3.12/site-packages/django/contrib/auth/urls.py
+    #  login/  [name='login']                                   # Перехвачен.
+    #  logout/  [name='logout']                                 # Перехвачен.
+    #  password_change/  [name='password_change']
+    #  password_change/done/  [name='password_change_done']
+    #  password_reset/  [name='password_reset']                 # Перехвачен.
+    #  password_reset/done/  [name='password_reset_done']
+    #  reset/<uidb64>/<token>/  [name='password_reset_confirm'] # Перехвачен.
+    #  reset/done/  [name='password_reset_complete']
 
     path('inspect/', views.UserInspectView.as_view(), name='inspect'),
 ]
