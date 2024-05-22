00a77fdf1a7e6d65f8fd660efc9c60594402b583
arfstudy <147665859+arfstudy@users.noreply.github.com>
2024-05-22 11:37:57 +0500

    Регистрирует нового пользователя в системе.
    Задаёт шаблон пути для активации процедуры создания нового пользователя.
    Создаёт необходимое представление регистрации и шаблон для отображения формы регистрации.
    Выполняет подтверждение почты и аутентификацию в системе.
    
    Changes committed:
        1. modified:   reference/my_diplom/templates/registration/email_verify_confirm.html
        2. new file:   reference/my_diplom/templates/registration/register.html
        3. modified:   reference/my_diplom/templates/webauth/home.html
        4. modified:   reference/my_diplom/webauth/forms.py
        5. modified:   reference/my_diplom/webauth/services.py
        6. modified:   reference/my_diplom/webauth/urls.py
        7. modified:   reference/my_diplom/webauth/views.py

--- reference/my_diplom/webauth/services.py
+++ reference/my_diplom/webauth/services.py
@@ -1,22 +1,25 @@
 from django.contrib.auth import login
 
 from users.emails import verify_received_keys, describe_keys_verify_result
+from users.services import delete_tentative_user
 
 
 def verify_received_email(request, key64, token):
     """ Выполняет подтверждение электронной почты на основании ключа и токена
         из ссылки из контрольного письма пользователя.
     """
-    actions = ['login']
+    actions = ['login', 'register']
     data, is_verify = verify_received_keys(request, key64, token, actions)
 
     data = describe_keys_verify_result(data, is_verify)
 
-    context = {'msg': data[data['process']]}
+    context = {'msg': data[data['process']], 'not_verify': not is_verify, 'is_register': data['process'] == 'register'}
     if is_verify:
         context['header'] = 'Электронная почта подтверждена'
         login(request, data['user'])
     else:
         context['header'] = 'Ошибка подтверждения почты'
+        if data['process'] == 'register' and 'user' in data.keys():
+            delete_tentative_user(data['user'])
 
     return context
