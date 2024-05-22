e05986d8a47c106c009b5a695abf1b44dc4153f0
arfstudy <147665859+arfstudy@users.noreply.github.com>
2024-05-22 09:23:47 +0500

    Проверяет ключ и токен. (Изменения приложения 'users'.)
    Создаёт файл 'emails.py' и меняет 'services.py' приложения 'users'.
    Создаёт функцию, отправляющую письмо с ключами, и функцию, проверяющую
    полученные ключи от пользователя. Создаёт вспомогательные функции.
    Подстраивает функцию 'users/services/decoding_key64(request, key_64, actions)'
    для возможности возврата словаря с ошибками.
    
    Changes committed:
        1. new file:   reference/my_diplom/users/emails.py
        2. modified:   reference/my_diplom/users/services.py

--- reference/my_diplom/users/services.py
+++ reference/my_diplom/users/services.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.utils.http import urlsafe_base64_decode
 
 User = get_user_model()
 
@@ -19,3 +20,41 @@ def get_user(param):
         return None
 
     return user
+
+
+def get_user_uidb64(uidb64):
+    """ Получает пользователя 'user' по ключу 'uidb64'.
+    """
+    #   urlsafe_base64_decode() decodes to bytestring
+    try:
+        user_id = urlsafe_base64_decode(uidb64).decode()
+    except (UnicodeDecodeError, TypeError, ValueError, OverflowError, ValidationError):
+        return None
+
+    return get_user(param=user_id)
+
+
+def decoding_key64(request, key_64, actions):
+    """ Выделяет действие, вызвавшее отправку электронного письма.
+        Определяет пользователя, по ключу.
+    """
+    process, key64 = 'process_err', 'key64'
+    for act in actions:
+        if key_64.startswith(act):
+            process = act
+            key64 = key_64.replace(act, '')
+            break
+
+    data = {'process': process}
+    if process == 'process_err':
+        data['process_err'] = 'Ваше действие нераспознано (неправильный ключ Key64).'
+        data['user_err'] = 'Определение пользователя прервано (неправильный ключ Key64).'
+        return data, False
+
+    user = get_user_uidb64(key64)
+    if user is None:
+        data['user_err'] = 'Вас не удалось идентифицировать (неправильный ключ Key64).'
+        return data, False
+
+    data['user'] = user
+    return data, True
