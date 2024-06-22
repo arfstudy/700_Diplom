7d49ecfc304d715b2557d720910c797c7eacf82e
arfstudy <147665859+arfstudy@users.noreply.github.com>
2024-06-22 09:52:54 +0500

    Подключает модель контактов к сайту администратора.
    Changes committed:
        1. modified:   reference/my_diplom/backend/admin.py

--- reference/my_diplom/backend/admin.py
+++ reference/my_diplom/backend/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend import models
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """ Класс для отображения контактов пользователей в административной панеле.
+    """
+    list_display = ['id', 'salesman', 'phone', 'city', 'street', 'house', 'structure', 'building', 'apartment']
+    list_display_links = ['id', 'salesman']
+    list_filter = ['salesman', 'city']
+    search_fields = ['phone', 'city', 'street', 'house', 'structure', 'building', 'apartment']
+    list_editable = ['street', 'house', 'structure', 'building', 'apartment']
