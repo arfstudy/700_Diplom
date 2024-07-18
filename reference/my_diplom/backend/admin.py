161170a1bbfd4bbe5e233f2f2738cfa927d840a2
arfstudy <147665859+arfstudy@users.noreply.github.com>
2024-07-18 22:29:52 +0500

    Подключает модели товара и заказа к сайту администратора.
    Changes committed:
        1. modified:   reference/my_diplom/backend/admin.py

--- reference/my_diplom/backend/admin.py
+++ reference/my_diplom/backend/admin.py
@@ -23,3 +23,74 @@ class ShopAdmin(admin.ModelAdmin):
     list_filter = ['state']
     search_fields = ['name', 'filename']
     ordering = ['-id']
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """ Класс для отображения категорий товаров в административной панеле.
+    """
+    list_display = ['id', 'name', 'catalog_number']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'catalog_number']
+    ordering = ['-id']
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    """ Класс для отображения товаров в административной панеле.
+    """
+    list_display = ['id', 'name', 'category']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    ordering = ['-id']
+
+
+@admin.register(models.ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    """ Класс для отображения описания товаров в административной панеле.
+    """
+    list_display = ['id', 'product', 'model', 'catalog_number', 'shop', 'quantity', 'price', 'price_rrc']
+    list_display_links = ['id', 'model']
+    search_fields = ['model', 'catalog_number']
+    ordering = ['-id']
+
+
+@admin.register(models.Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    """ Класс для отображения характеристик товаров в административной панеле.
+    """
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    ordering = ['-id']
+
+
+@admin.register(models.ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    """ Класс для отображения значений характеристики товара в административной панеле.
+    """
+    list_display = ['id', 'parameter', 'value', 'product_info']
+    list_filter = ['parameter', 'product_info']
+    search_fields = ['value']
+    list_editable = ['value']
+    ordering = ['-id']
+
+
+class OrderItemInLine(admin.TabularInline):
+    """ Класс для отображения товаров из заказа в административной панеле.
+        Промежуточная таблица с дополнительными полями, встраиваемая в отображение ведущей.
+    """
+    model = models.OrderItem
+    extra = 0
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    """ Класс для отображения заказов в административной панеле.
+    """
+    list_display = ['id', 'state', 'updated_state', 'contact', 'customer', 'created_at']
+    list_display_links = ['id', 'updated_state', 'created_at']
+    list_filter = ['state', 'customer']
+    search_fields = ['updated_state', 'created_at']
+    ordering = ['-id']
+    inlines = [OrderItemInLine]
