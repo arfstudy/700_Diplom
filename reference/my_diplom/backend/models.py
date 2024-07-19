a50e6ebf2c3bee24c3751860ce7501293fc1fd28
arfstudy <147665859+arfstudy@users.noreply.github.com>
2024-07-19 20:12:58 +0500

    Задаёт все модели для товара и заказа.
    Задаёт модели, выполняет миграции.
    
    Changes committed:
        1. new file:   reference/my_diplom/backend/migrations/0003_parameter_category_order_product_orderitem_and_more.py
        2. modified:   reference/my_diplom/backend/models.py

--- reference/my_diplom/backend/models.py
+++ reference/my_diplom/backend/models.py
@@ -77,3 +77,164 @@ class Shop(models.Model):
             self.state = Shop.Worked.CLOSE
 
         return super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    """ Категория товара.
+    """
+    name = models.CharField(max_length=40, unique=True, verbose_name='Название')
+    catalog_number = models.IntegerField(verbose_name='Номер по каталогу')
+    shops = models.ManyToManyField(to=Shop, blank=True, related_name='categories', verbose_name='Магазины')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Список категорий'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """ Продукт.
+    """
+    name = models.CharField(max_length=80, unique=True, verbose_name='Название')
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, null=True, blank=True, related_name='products',
+                                 verbose_name='Категория')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = "Список продуктов"
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductInfo(models.Model):
+    """ Описание товара.
+    """
+    model = models.CharField(max_length=80, verbose_name='Модель')
+    catalog_number = models.PositiveIntegerField(verbose_name='Номер по каталогу')
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, null=True, blank=True,
+                                related_name='product_infos', verbose_name='Продукт')
+    shop = models.ForeignKey(to=Shop, on_delete=models.CASCADE, null=True, blank=True, related_name='product_infos',
+                             verbose_name='Магазин')
+    quantity = models.PositiveIntegerField(null=True, blank=True, verbose_name='Количество')
+    price = models.PositiveIntegerField(null=True, blank=True, verbose_name='Закупочная цена')
+    price_rrc = models.PositiveIntegerField(null=True, blank=True, verbose_name='Рекомендуемая розничная цена')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Информация о продукте'
+        verbose_name_plural = "Информационный список о продуктах"
+        constraints = [
+            models.UniqueConstraint(fields=['product', 'shop', 'catalog_number'], name='unique_product_info'),
+        ]
+
+    def __str__(self):
+        return f'{self.product.name} {self.model}'
+
+
+class Parameter(models.Model):
+    """ Параметр товара.
+    """
+    name = models.CharField(max_length=40, unique=True, verbose_name='Название параметра')
+    products = models.ManyToManyField(to=ProductInfo, through='ProductParameter', blank=True,
+                                      related_name='parameters', verbose_name='Магазины')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Имя параметра'
+        verbose_name_plural = "Список имён параметров"
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductParameter(models.Model):
+    """ Значение параметра товара.
+    """
+    product_info = models.ForeignKey(to=ProductInfo, on_delete=models.CASCADE, related_name='product_parameters',
+                                     verbose_name='Описание товара')
+    parameter = models.ForeignKey(to=Parameter, on_delete=models.CASCADE, related_name='product_parameters',
+                                  verbose_name='Название параметра')
+    value = models.CharField(max_length=100, verbose_name='Значение параметра')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Значение параметра'
+        verbose_name_plural = "Список значений параметров"
+        constraints = [
+            models.UniqueConstraint(fields=['product_info', 'parameter'], name='unique_product_parameter'),
+        ]
+
+
+class Order(models.Model):
+    """ Заказ.
+    """
+    class Status(models.TextChoices):
+        """ Статусы состояния заказа. """
+        BASKET = 'BS', 'В корзине'
+        NEW = 'NE', 'Новый'
+        """ 1) На данный момент Вам недоступно изменение заказа. Нужно отменить заказ.
+            2) Ваш заказ отменён.
+        """
+        CONFIRMED = 'CF', 'Подтверждён'
+        ASSEMBLED = 'AS', 'Собран'
+        SENT = 'SN', 'Отправлен'
+        CANCELED = 'CN', 'Отменён'
+        # DELIVERED = 'DV', 'Доставлен'    # Заменил на "Получен".
+        RECEIVED = 'RC', 'Получен'
+
+    customer = models.ForeignKey(to=Salesman, on_delete=models.CASCADE, related_name='orders',
+                                 verbose_name='Покупатель')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    state = models.CharField(max_length=2, choices=Status.choices, default=Status.BASKET, verbose_name='Статус')
+    updated_state = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE, null=True, blank=True,
+                                related_name='orders', verbose_name='Адрес доставки')
+    product_infos = models.ManyToManyField(to=ProductInfo, through='OrderItem', related_name='orders',
+                                          verbose_name='Магазины')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = "Список заказов"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def sum(self):
+        # Вычисляет сумму заказа.
+        return self.ordered_items.aggregate(
+            total=models.Sum(models.F('product_info__price_rrc')*models.F('quantity')))['total']
+
+
+class OrderItem(models.Model):
+    """ Позиция заказа.
+    """
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name='ordered_items', verbose_name='Заказ')
+    product_info = models.ForeignKey(to=ProductInfo, on_delete=models.CASCADE, related_name='ordered_items',
+                                verbose_name='Продукт')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Заказанная позиция'
+        verbose_name_plural = "Список заказанных позиций"
+        constraints = [
+            models.UniqueConstraint(fields=['order_id', 'product_info'], name='unique_order_item'),
+        ]
